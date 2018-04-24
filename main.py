6040b6e59b419426d81f1645c1255b47ae8e7de1
Unknown <joonas.malinen@hotmail.fi>
2018-04-24 19:03:32 +0300

    Added udp client

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
+import server
+
 
 client = discord.Client()
 client = commands.Bot(command_prefix="/")
@@ -11,12 +13,16 @@ async def on_ready():
 @client.command(pass_context=True)
 async def test(ctx):
     print("penis")
-    #await client.say("pong", tts=True)
+    await client.say("pong")
+
+@client.command(pass_context=True)
+async def bileet(ctx):
+    await client.say("Are you coming to a party?")
 
 @client.event
 async def on_voice_state_update(before, after):
-    channel = list(client.servers)[0]
-    print(channel)
+    channel = list(client.get_all_channels())
+    #print(channel)
     #await client.send_message(channel,  "joined the channel", tts=True)
 
 
@@ -28,5 +34,43 @@ async def on_message(msg): #msg.content löytää tekstin mitä viestissä on
         print(msg.channel)
     await client.process_commands(msg) #So that commands may be used
 
-client.run("MzUxMjkzNDU3NjEzOTc5NjU4.DIQflg.mUXZck40bzRLg9HOA9QC48nWSTs")
+@client.event
+async def on_reaction_add(reaction, user):
+    text = "Tervetuloa bileisiin " + user.nick
+    #print(reaction.message.channel)
+    if reaction.message.author.name == "Joukkue-Bot":
+        await client.send_message(reaction.message.channel, text)
+
+import socket
+import sys
+import _thread
+
+def udp_client(asd):
+    HOST = "127.0.0.1"
+    PORT = 80
+
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print("Client socket created")
+    except socket.error as msg:
+        print("Client failed to create socket. Error: " + str(msg))
+        sys.exit()
+    s.bind((HOST, PORT))
+
+    while True:
+        try:
+            d = s.recvfrom(1024)
+            reply = d[0].decode()
+            addr = d[1]
+            client.send_message("botplayground", reply)
+            print("Server reply: " + reply)
+        except socket.error as msg:
+            print("Error Code: " + str(msg[0]) + " Message: " + msg[1])
+            sys.exit()
+
+
+
+_thread.start_new_thread(udp_client, ("client-1",))
+
+client.run("MzUxMjkzNDU3NjEzOTc5NjU4.DX65NA.sqfgxgvL9aaaX0zSlvxBoWckx0M")
 
